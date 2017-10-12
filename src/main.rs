8fb4d603b8dccbb356cc003e490765ea596a22d2
Sean Prashad <S.Prashad@outlook.com>
2017-10-12 14:34:18 -0400

    Initial structure of Cargo project

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
+extern crate sha1;
+
 use std::path::Path;    // For use in getFileName()
 use std::ffi::OsString; // For use in getFileName()
-use std::fs;            // For use in getFileSize()
 
 fn main(){
     let path = Path::new("foo.txt");
 
     // Optional values - refer to: https://doc.rust-lang.org/std/option/ 
     println!("1. {:?}", get_file_name(path)); // unwrap() supresses the "Some" keyword
-    println!("2. {:?}", get_file_size(path));
+    //println!("2. {:?}", get_file_size(path));
+
+
+
+    let m = sha1::Sha1::new();
+
+    let s = String::from(m.digest().to_string());
+
+    println!("{:?}", s);
+
+    //let mut m = sha1::Sha1::new();
+    //m.update(b"Hello World!");
+    // println!("3. {:?}", generateSHA(path));
+
+    //assert_eq!(m.digest().to_string(), "2ef7bde608ce5404e97d5f042f95f89f1c232871");
 }
+
+// fn generateSHA(file_path: &Path) -> ! {
+//     let mut m = sha1::Sha1::new();
+
+//     let s = String::from(m);
+
+//     //m.update(OsString::from(file_path.file_name()));
+// }
 
 // Unlike C/C++, there's NO restrictions on the order of function definitions!
 
@@ -23,14 +46,14 @@ fn get_file_name(file_path: &Path) -> OsString {
 /*
     getFileSize(Path file_path) -> OsString
  */
-fn get_file_size(file_path: &Path) -> fs::Metadata {
-    let metadata = fs::metadata(file_path);
+//fn get_file_size(file_path: &Path) -> fs::Metadata {
+    // let metadata = fs::metadata(file_path);
     //return OsString::from(metadata);
     //return fs::metadata(filePath).unwrap();
 
-    let metadata = fs::metadata("foo.txt");
-    //assert!(!metadata.is_dir());
-    return metadata.file_type.unwrap();
+    // let metadata = fs::metadata("foo.txt");
+    // //assert!(!metadata.is_dir());
+    // return metadata.file_type.unwrap();
 
     //assert_eq!(0, metadata.len());
-}
+//}

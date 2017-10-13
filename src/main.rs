f4cf2ac356c7dfbf011eabf0fa2057515b1faae7
Sean Prashad <S.Prashad@outlook.com>
2017-10-12 20:39:37 -0400

    Fix #4: Implement generate_md5() function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-extern crate sha1;
+extern crate sha1;  // For use of SHA1 functions
+extern crate md5;   // For use of MD5 functions
 
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
 use std::path::Path;
+use std::io::prelude::*;
 
 /*
     To run program: cargo run [File_Name_Goes_Here_As_Command_Line_Argument]
@@ -13,16 +14,15 @@ use std::path::Path;
 fn main(){
     let args: Vec<String> = env::args().collect();
 
-    /*
-        File I/O: https://doc.rust-lang.org/book/second-edition/ch12-02-reading-a-file.html
-    */
+    // File I/O: https://doc.rust-lang.org/book/second-edition/ch12-02-reading-a-file.html
     get_file_name(&args[1]);
     generate_sha(&args[1]);
+    generate_md5(&args[1]);
 }
 
 fn get_file_name(file_path: &String) -> () {
     let path = Path::new(file_path);
-    println!("1. {:?}", path.file_name().unwrap());
+    println!("File name: {:?}", path.file_name().unwrap());
 }
 
 fn generate_sha(file_path: &String) -> () {
@@ -31,10 +31,21 @@ fn generate_sha(file_path: &String) -> () {
     let mut file_contents = String::new();
     file.read_to_string(&mut file_contents).expect("Something went wrong reading the file!");
 
-    let mut m = sha1::Sha1::new();
-    m.update(file_contents.as_bytes());
+    let mut sha_hash = sha1::Sha1::new();
+    sha_hash.update(file_contents.as_bytes());
 
-    println!("2. After SHA1 hash: {:?}", m.digest().to_string());
+    println!("SHA1 hash: {:?}", sha_hash.digest().to_string());
+}
+
+fn generate_md5(file_path: &String) -> () {
+    let mut file = File::open(file_path).expect("File Not Found!");
+
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents).expect("Something went wrong reading the file!");
+
+    let md5 = md5::compute(file_contents);
+
+    println!("MD5 hash: \"{:x}\"", md5);
 }
 
 /*

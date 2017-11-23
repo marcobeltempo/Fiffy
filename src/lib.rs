2b8f3f731188dfb021e5a3cd23bcd479bb7cd4ac
Sean Prashad <S.Prashad@outlook.com>
2017-11-23 18:47:56 -0500

    Add crypto library & implement sha1/md5 unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
-// Run program locally with `cargo build --features "clippy";`
+// Run program locally with: cargo build --features "clippy";
 
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
-extern crate sha1;
-extern crate md5;
+extern crate crypto;
 
 use std::fs;
-use std::str;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 use std::ffi::OsString;
-use std::ops::Deref;
+use self::crypto::digest::Digest;
+use self::crypto::sha1::Sha1;
+use self::crypto::md5::Md5;
 
 pub fn get_file_name(file_path: &str) -> OsString {
     let path = Path::new(file_path.trim());
@@ -30,7 +30,7 @@ pub fn get_file_size(file_path: &str) -> u64 {
     }
 }
 
-pub fn generate_sha1(file_path: &str) -> sha1::Sha1 {
+pub fn generate_sha1(file_path: &str) -> String {
     let mut file = File::open(file_path.trim()).expect("File Not Found!");
     let mut file_contents = String::new();
 
@@ -38,13 +38,13 @@ pub fn generate_sha1(file_path: &str) -> sha1::Sha1 {
         "Something went wrong reading the file!"
     );
 
-    let mut sha_hash = sha1::Sha1::new();
-    sha_hash.update(file_contents.as_bytes());
-
-    sha_hash
+    let mut sha_hash = Sha1::new();
+    sha_hash.input_str(&file_contents.to_string());
+    
+    sha_hash.result_str()
 }
 
-pub fn generate_md5(file_path: &str) -> &str {
+pub fn generate_md5(file_path: &str) -> String {
     let mut file = File::open(file_path.trim()).expect("File Not Found!");
     let mut file_contents = String::new();
 
@@ -52,16 +52,15 @@ pub fn generate_md5(file_path: &str) -> &str {
         "Something went wrong reading the file!"
     );
 
-    let x = md5::compute(file_contents);
-
-    println!("Calculated MD5 digest is: {:?}", x.deref());
-
-    "placeholder"
+    let mut md5_hash = Md5::new();
+    md5_hash.input_str(&file_contents.to_string());
+    
+    md5_hash.result_str()
 }
 
-// To run tests, execute `cargo test` whilst inside the repo file structure
+// To run tests, execute `cargo test`
 #[cfg(test)]
-pub mod tests {
+mod tests {
     #[test]
     fn get_file_name() {
         use std::ffi::OsString;
@@ -69,7 +68,8 @@ pub mod tests {
 
         let file_name = OsString::from("lazydog.txt");
 
-        assert_eq!(get_file_name("./lazydog.txt"), file_name);
+        assert_eq!(get_file_name("/Desktop/lazydog.txt"), file_name);
+        assert_eq!(get_file_name("/Desktop/lazydog.txt").len(), file_name.len());
     }
 
     #[test]
@@ -78,35 +78,26 @@ pub mod tests {
 
         let file_size : u64 = 43;
 
-        assert_eq!(get_file_size("./lazydog.txt"), file_size);
+        assert_eq!(get_file_size("lazydog.txt"), file_size);
     }
 
     #[test]
     fn calc_sha1() {
-        // use sha1;
+        use ::generate_sha1;
 
-        // let mut m = sha1::Sha1::new();
+        let sha1_hash = String::from("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
 
-        // m.reset();
-        // m.update("The quick brown ".as_bytes());
-        // m.update("fox jumps over ".as_bytes());
-        // m.update("the lazy dog".as_bytes());
-
-        // // Create a string using my functions and assert that below!
-
-        // let hh = m.digest().to_string();
-
-        // let h = "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
-        // assert_eq!(hh.len(), h.len());
-        // assert_eq!(hh, &*h);
+        assert_eq!(generate_sha1("lazydog.txt"), sha1_hash);
+        assert_eq!(generate_sha1("lazydog.txt").len(), sha1_hash.len());
     }
 
     #[test]
     fn calc_md5() {
-        // use ::generate_md5;
+        use ::generate_md5;
 
-        // let string_val = String::from("lazydog.txt");
+        let md5_hash = String::from("9e107d9d372bb6826bd81d3542a419d6");
 
-        // assert_eq!(generate_md5(&string_val), "c3fcd3d76192e4007dfb496cca67e13b");
+        assert_eq!(generate_md5("lazydog.txt"), md5_hash);
+        assert_eq!(generate_md5("lazydog.txt").len(), md5_hash.len());;
     }
 }
